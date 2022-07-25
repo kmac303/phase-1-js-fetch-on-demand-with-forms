ac25eae2fa08a4a418709fe9756972f098cff00d
Kevin McIntosh <kmac303@gmail.com>
2022-07-25 14:03:26 -0600

    commit tree d00de711b5caa65496b5aa1822bd153d35a7d7ce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-const init = () => {
-  
-}
+// const init = () => {
+//     const inputForm = document.querySelector('form');
+//     inputForm.addEventListener('submit', (event) => {
+//         event.preventDefault();
+//         console.log(event);
+//     });
+// }
 
-document.addEventListener('DOMContentLoaded', init);
+// document.addEventListener('DOMContentLoaded', init);
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      console.log(input.value);
+  
+      fetch('http://localhost:3000/movies')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // LOG: (3) [{…}, {…}, {…}]
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
